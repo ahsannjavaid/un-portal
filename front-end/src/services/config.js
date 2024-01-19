5a63889f0d238ca299a8f63a0f8d2456fb94edfb
Ahsan Javed <ahsannjavaid@gmail.com>
2024-01-19 11:44:50 +0500

    backend v2

--- front-end/src/services/config.js
+++
@@ -1,2 +0,0 @@
-// export const BASE_URL = "https://unportal.onrender.com/"
-export const BASE_URL = "http://localhost:5000/";
--- front-end/src/services/endpoints/examEndpoints.js
+++
@@ -1,9 +0,0 @@
-import { BASE_URL } from "../config";
-
-export const examEndpoints = {
-  postMarks: () => `${BASE_URL}marks`,
-
-  getMarks: () => `${BASE_URL}marks-details`,
-
-  getParticularMarks: (email) => `${BASE_URL}get-particular-marks?email=${email}`,
-};
